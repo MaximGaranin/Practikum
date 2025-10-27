d676ef26a94c724d9869ba622b41c7ac529aed15
Garanin Maxim <lmibuh3@yandex.ru>
2025-10-27 10:33:37 +0400

    Pofile, Task, Settings

--- Practikum/practikum/urls.py
+++ Practikum/practikum/urls.py
@@ -6,4 +6,7 @@ app_name = 'prac'
 urlpatterns = [
     path('', views.course, name='course'),
     path('course/<int:course_id>/', views.course_program, name='course_program'),
+    path('profile/', views.profile, name='profile'),
+    path('task/', views.task, name='task'),
+    path('settings/', views.settings, name='settings')
 ]
--- Practikum/practikum/views.py
+++ Practikum/practikum/views.py
@@ -16,3 +16,18 @@ def course_program(request, course_id=None):
         'course_id': course_id,
     }
     return render(request, 'course/course_program.html', context)
+
+
+def profile(request):
+    """Профиль."""
+    return render(request, 'profile/profile.html')
+
+
+def task(request):
+    """Задачи."""
+    return render(request, 'task/task.html')
+
+
+def settings(request):
+    """Настройки."""
+    return render(request, 'settings/settings.html')
