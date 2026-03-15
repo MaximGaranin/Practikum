73210c32fae67a70930f1a6818bac4c3b50d3e9f
MaximGaranin <lmibuh3@yandex.ru>
2026-03-15 22:05:29 +0400

    Fix

--- Practikum/Practikum/urls.py
+++ Practikum/Practikum/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from editor import views as editor_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('practikum.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('Logistic_Task.urls')),
-    path('api/', include('editor.urls')),
+    path('api/', include('editor.urls')),                              # /api/analyze/, /api/execute/
+    path('editor/', editor_views.editor_view, name='editor'),         # /editor/
+    path('editor/<int:task_id>/', editor_views.editor_view, name='editor_task'),  # /editor/5/
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- Practikum/editor/urls.py
+++ Practikum/editor/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'editor'
 
 urlpatterns = [
-    path('editor/', views.editor_view, name='editor'),
-    path('editor/<int:task_id>/', views.editor_view, name='editor_task'),
-    path('api/analyze/', views.analyze_code, name='analyze_code'),
-    path('api/execute/', views.execute_code, name='execute_code'),
+    path('analyze/', views.analyze_code, name='analyze_code'),
+    path('execute/', views.execute_code, name='execute_code'),
 ]
 
